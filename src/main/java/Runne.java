986a4d5c10795cf78fcb51674750f0a367cf5d58
Mr. Nobody <mrnobody128712@gmail.com>
2024-05-25 20:48:19 +0300

    v2

--- src/main/java/Runne.java
+++
@@ -1,8 +0,0 @@
-public class Runne {
-    public static void main(String[] args) {
-
-        //versiyon 1
-        System.out.println("hello Gıt");
-        System.out.println("git add . : " );
-    }
-}
